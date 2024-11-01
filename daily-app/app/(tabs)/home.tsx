67c879c720d3239f35bd3312ba0a6d60d15d3e03
Alyssa Gillen <alyssagillen1234@gmail.com>
2024-11-01 11:37:19 -0500

    Project initialization

---
+++ daily-app/app/(tabs)/home.tsx
@@ -0,0 +1,12 @@
+import {  StyleSheet, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    
+      <Text>Home</Text>
+
+  );
+}
+
+const styles = StyleSheet.create({
+});
